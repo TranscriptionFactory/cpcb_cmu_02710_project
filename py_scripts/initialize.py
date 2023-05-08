99b77110423b86f18b5143a07fbe8d324f048233
TF <transcriptionfactory@gmail.com>
2023-05-08 16:30:15 -0400

    with integrated data

--- py_scripts/initialize.py
+++ py_scripts/initialize.py
@@ -6,11 +6,11 @@ import subprocess
 
 import scdiff_init
 
-data_file = 'data/tcell_data2_kc6.E'
+data_file = 'integrated_data.E'
 
 tf_dna_path = '/net/dali/home/mscbio/aar126/genomics/jubilant-barnacle/scdiff/tf_dna/Mouse_TF_targets.txt'
 
 #initial clusters - number of time points? 
-kc = 6
+kc = 7
 
 scdiff_init.run_scdiff_init(data_file, tf_dna_path, kc)
